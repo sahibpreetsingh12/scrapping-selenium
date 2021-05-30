6064a053df2bd5a3ef4ecbc01ce891119d842d0a
sahib <ss9334931@gmail.com>
2021-05-30 22:23:58 +0530

    first commit

---
+++ tut1.py
@@ -0,0 +1,14 @@
+# download chrome driver from 
+# https://sites.google.com/a/chromium.org/chromedriver/downloads
+# unzip it and give it's path
+
+from selenium import webdriver
+
+# webdriver are basically used to provide you access to website so that you make 
+# actions to webiste like clicking any button or other oprations
+# path of webdriver 
+path = '/home/sahib/selenium-tut/chromedriver'
+
+driver = webdriver.Chrome(path)
+
+driver.get('https://www.youtube.com/')
